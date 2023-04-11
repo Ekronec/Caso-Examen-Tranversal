d12274b5cb6a877d1d3e0fa69c9a99f64b38e659
Ekronec <besa.eitan.r@gmail.com>
2023-04-11 12:18:46 -0400

    - Creacion de funciones - Error en añadir numero de compras al carro

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+
+function addItemNumber() {
+    var itemNumber = itemNumber + 1;
+    console.log(itemNumber)
+}
+
+function showNumber(){
+    return document.getElementsByName("itemNumber").values
+}
