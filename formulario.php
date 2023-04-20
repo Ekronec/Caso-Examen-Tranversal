eb804898b38f22159dfacb1eb675a9f3481f64a6
Ekronec <besa.eitan.r@gmail.com>
2023-04-20 12:39:01 -0400

    -Se annadio jquary -Se intento annadir php (fracaso)

--- formulario.php
+++ formulario.php
@@ -1,10 +1,23 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$emailUsuario = $_POST["emailUsuario"];
-	$password = $_POST["password"];
 
-	// Aquí iría el código para procesar el formulario, como enviar un correo electrónico o guardar los datos en una base de datos
+	$email = $password = "";
 
-	echo "¡Gracias por enviar el formulario!";
-}
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$email = test_input($_POST["emailUsuario"]);
+		$password = test_input($_POST["password"]);
+
+		// Aquí iría el código para procesar el formulario, como enviar un correo electrónico o guardar los datos en una base de datos
+
+		echo "¡Gracias por enviar el formulario!";
+	}
+
+
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+
 ?>
