fae742d526066be5bc2313f599a0d590d1f9cb45
Ekronec <besa.eitan.r@gmail.com>
2023-04-27 11:47:10 -0400

    cambio de nombres

--- formulario.php
+++ formulario.php
@@ -3,13 +3,10 @@
 $emailUsuario = $_POST['emailUsuario'];
 $password = $_POST['password'];
 
-// Comprobar si el usuario y la contraseña son correctos
 if ($emailUsuario === 'emailUsuario' && $password === 'password') {
-  // Si son correctos, redirigir al usuario a la página de inicio
   header('Location: index.php');
   exit;
 } else {
-  // Si no son correctos, mostrar un mensaje de error
   echo 'El nombre de usuario o la contraseña son incorrectos.';
 }
 
