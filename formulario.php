18dc0f2a2866eece3a268356657934cb2191ea7f
Ekronec <besa.eitan.r@gmail.com>
2023-04-27 12:17:03 -0400

    Funciona el formulario!!!!!!!!!!!!!!!!!!!!!!!!!!

--- formulario.php
+++ formulario.php
@@ -5,7 +5,6 @@ $password = $_POST['password'];
 
 if ($emailUsuario === 'emailUsuario' && $password === 'password') {
   header('Location: index.php');
-  exit;
 } else {
   echo 'El nombre de usuario o la contraseña son incorrectos.';
 }
