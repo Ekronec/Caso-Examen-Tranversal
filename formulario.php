ad0b5e41c8784f4a337a9fb06922e10c3c909b57
Ekronec <besa.eitan.r@gmail.com>
2023-04-27 11:34:16 -0400

    Cambio en formulario pero sigue sin funcionar

--- formulario.php
+++ formulario.php
@@ -1,23 +1,16 @@
 <?php
 
-	$email = $password = "";
+$emailUsuario = $_POST['emailUsuario'];
+$password = $_POST['password'];
 
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$email = test_input($_POST["emailUsuario"]);
-		$password = test_input($_POST["password"]);
-
-		// Aquí iría el código para procesar el formulario, como enviar un correo electrónico o guardar los datos en una base de datos
-
-		echo "¡Gracias por enviar el formulario!";
-	}
-
-
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-
+// Comprobar si el usuario y la contraseña son correctos
+if ($emailUsuario === 'emailUsuario' && $password === 'password') {
+  // Si son correctos, redirigir al usuario a la página de inicio
+  header('Location: index.php');
+  exit;
+} else {
+  // Si no son correctos, mostrar un mensaje de error
+  echo 'El nombre de usuario o la contraseña son incorrectos.';
+}
 
 ?>
