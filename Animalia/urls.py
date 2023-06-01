4f1d698b29ca7963e8365bc8c22569d61b62c58e
Ekronec <besa.eitan.r@gmail.com>
2023-06-01 12:35:59 -0400

    - Intento de implementacion del css de login (fracaso) - Se arreglaron algunas redirecciones para que funcionen en django

--- Animalia/urls.py
+++ Animalia/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('index',views.index, name='index'),
     path('signin',views.signin, name='signin'),
+    path('Dog',views.Dog, name='Dog'),
+    path('login',views.login, name='login'),
+    path('Cat',views.Cat, name='Cat'),
+    path('aquatic',views.aquatic, name='aquatic'),
+    path('cssLogin',views.cssLogin, name='cssLogin'),
 ]
--- Animalia/views.py
+++ Animalia/views.py
@@ -9,9 +9,40 @@ def index(request):
     
     return render(request, "pages/index.html", context)
 
+def login(request):
+    usuario = UserAnimalia.objects.all()
+    context = {"user": usuario}
+    
+    return render(request, "pages/login.html", context)
+
+def Dog(request):
+    usuario = UserAnimalia.objects.all()
+    context = {"user": usuario}
+    
+    return render(request, "pages/Dog.html", context)
+
+def Cat(request):
+    usuario = UserAnimalia.objects.all()
+    context = {"user": usuario}
+    
+    return render(request, "pages/Cat.html", context)
+
+def aquatic(request):
+    usuario = UserAnimalia.objects.all()
+    context = {"user": usuario}
+    
+    return render(request, "pages/aquatic.html", context)
+
 
 def signin(request):
     user = UserAnimalia.objects.all()
     context = {"user" : user}
     
     return render(request, 'pages/signin.html', context)
+
+
+def cssLogin(request):
+    usuario = UserAnimalia.objects.all()
+    context = {"user": usuario}
+    
+    return render(request, "pages/css/signin.css", context)
